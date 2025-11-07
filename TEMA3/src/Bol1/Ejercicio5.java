524dbefc7e901246a20e3e96a6890477a2b763a3
Ibrahim-prog-0 <ibrahim.taib.105.105@gmail.com>
2025-11-07 12:45:45 +0100

    He continuado con los ejercicios

--- TEMA3/src/Bol1/Ejercicio5.java
+++ TEMA3/src/Bol1/Ejercicio5.java
@@ -1,17 +1,29 @@
 package Bol1;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Escriba el primer numero.");
+		int num1 = sc.nextInt();
+
+		System.out.println("Ahora el segundo.");
+		int num2 = sc.nextInt();
+
+		System.out.println("Por ultimo, el tercero.");
+		int num3 = sc.nextInt();
 		
-		
-		
-		
-		
-		
-		
-		
+		if (Ejercicio4.mayor(num1, num2) > num3) {
+			System.out.println("El numero mayor es " + Ejercicio4.mayor(num1, num2) + ".");
+		} else {
+			System.out.println("El numero mayor es " + num3 + ".");
+		}
+		sc.close();
+
 	}
-	
+
 }
--- TEMA3/src/Bol1/Ejercicio6.java
+++ TEMA3/src/Bol1/Ejercicio6.java
@@ -1,10 +1,34 @@
 package Bol1;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la letra para saber si se trata de una vocal.");
+		char letra = sc.next().charAt(0);
+		char letraEnMayus = Character.toUpperCase(letra);
+		if (vocal(letraEnMayus) ==true) {
+			System.out.println("La letra es una vocal");
+		} else {
+			System.out.println("La letra es consonante");
+		}
+		
+		sc.close();
+	}
+	public static boolean vocal(char vocalSiNo) {
+		switch (vocalSiNo) {
+		case 'A', 'E', 'I', 'O', 'U':
+			return true;
+			
+
+		default:
+			return false;
+			
+		}
+		
 		
 	}
-
 }
